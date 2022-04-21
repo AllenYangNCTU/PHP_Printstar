8d760d08dfe036ac24398156598e239aad244b2c
Yanghejie <yanghejie.bt04@nycu.edu.tw>
2022-04-21 16:34:00 +0800

    initial commit

---
+++ printstar.php
@@ -0,0 +1,28 @@
+<?php
+$N=10;
+$sp=$N;
+$star=1;
+$counter=$N*2;
+while($counter>-2){
+    if($counter>$N){
+        print(str_repeat("&nbsp&nbsp",$sp));
+        print(str_repeat("*",$star)); 
+        print("<br>");
+        $sp--;
+        $star+=2;
+    }
+    else if($counter==$N) {
+        print(str_repeat("*",$N*2+1));
+        print("<br>");
+        $counter--;
+    }
+    else if($counter>-2 && $counter<$N){
+        $star=$star-2;
+        $sp++;
+        print(str_repeat("&nbsp&nbsp",$sp));
+        print(str_repeat("*",$star)); 
+        print("<br>");
+        }
+    $counter--; 
+}
+?>
